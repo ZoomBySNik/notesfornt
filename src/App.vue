87aa66b5eba9808fda6e2532e8a3ed8dc5f08842
ZoomBySNik <Onyx1504@yandex.ru>
2023-06-12 13:04:36 +0300

    Создает проект заметки

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import NoteInGrid from "./components/NoteInGrid.vue";
+</script>
+
+<template>
+  <header>
+    <button class="burger"></button>
+    <div class="search-field"><input type="text" class="search-input"></div>
+    <button class="login"></button>
+  </header>
+  <div class="grid-for-notes"><NoteInGrid></NoteInGrid></div>
+</template>
+<style scoped>
+header{
+  height: 4em;
+  width: auto;
+  background: #D9D9D9;
+  padding:  0.5em 3.6em;
+}
+.burger{
+  background-image: url("./assets/hamburger.png");
+  background-size: contain;
+  background-color: transparent;
+  border: none;
+  width: 2.25em;
+  height: 2.25em;
+  margin: 0.875em 0;
+  display: inline-block;
+}
+.search-field{
+  display: inline-block;
+}
+.grid-for-notes{
+  padding:  3.6em;
+}
+</style>
---
+++ src/components/NoteInGrid.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="note">
+    <div class="header-of-note">
+      <h1 class="note-title">Title</h1>
+      <button class="select-button">btn</button>
+    </div>
+    <p class="note-content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada fames ac turpis egestas integer.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NoteInGrid"
+}
+</script>
+
+<style scoped>
+
+</style>
