569967fa6ca09f18ace10ad2e875369ab86e619d
ZoomBySNik <Onyx1504@yandex.ru>
2023-06-16 13:31:57 +0300

    Реализовано меню для папок

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,49 @@
 <template>
   <header class="for-row">
     <div class="flex1">
-      <button class="burger"></button>
-      <button class="plus"></button>
+      <button class="burger" @click="toggleMenu"></button>
+      <button class="add-note-button"></button>
     </div>
     <div class="flex2">
       <div class="search-field">
-        <input type="text" class="search-input" placeholder="Поиск по тексту">
+        <input type="text" class="search-input text-input" placeholder="Поиск по тексту">
         <button class="search-button"></button>
       </div>
     </div>
     <div class="flex3"><button class="login"></button></div>
   </header>
+  <ul class="hidden-menu" v-if="isMenuOpen">
+    <li @click="toggleMenu"><a>Label1</a></li>
+    <li @click="toggleMenu"><a>Label2</a></li>
+    <li class="for-row">
+      <input type="text" placeholder="Добавить папку" class="text-input tag-add-input">
+      <button class="add-label-button"></button>
+    </li>
+  </ul>
 </template>
 
 <script>
+
 export default {
-  name: "Navbar"
+  data() {
+    return {
+      isMenuOpen: this.isOpen
+    };
+  },
+  name: "Navbar",
+  methods:{
+    toggleMenu: function () {
+      this.isMenuOpen = !this.isMenuOpen;
+      console.log('toggleMenu');
+    },
+  },
+  props:{
+    isOpen: {
+      type:Boolean,
+      required: false,
+      default: false
+    },
+  }
 }
 </script>
 
@@ -51,7 +78,7 @@ header{
   height: 2em;
   margin: 0.4em 0;
 }
-.plus{
+.add-note-button{
   background-image: url("../assets/free-icon-pencil-4409398.png");
   background-size: 80%;
   background-repeat: no-repeat;
@@ -69,8 +96,6 @@ header{
   display: flex;
 }
 .search-input{
-  background-color: transparent;
-  border: none;
   height: 2em;
   width: 100%;
   margin: 0.5em 0 0.5em 0.5em;
@@ -97,5 +122,40 @@ header{
   width: 3em;
   height: 3em;
   border-radius: 100%;
+}
+.hidden-menu {
+  margin-left: 1.9em;
+  margin-top: 2em;
+  width: 20%;
+  position: fixed;
+  list-style: none;
+  border-radius: 1em;
+  background-color: #D9D9D9;
+  box-shadow: 0 0 0.8em rgba(0, 0, 0, 0.6);
+  padding: 0.4em;
+  color: #1E282E;
+  z-index: 1000;
+}
+.hidden-menu li {
+  padding: 0.4em;
+}
+.hidden-menu li a{
+  color: inherit;
+  text-decoration: none;
+}
+.add-label-button{
+  background-image: url("../assets/free-icon-font-plus-3917757.png");
+  background-size: 80%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-color: transparent;
+  border: none;
+  width: 1.2em;
+  height: 1.2em;
+}
+
+.tag-add-input {
+  width: 90%;
+  text-align: left;
 }
 </style>
